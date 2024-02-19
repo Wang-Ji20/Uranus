3d7e79670d15b7ba9d8a07fed09a9a0ea7a6d6f4
hortensia <824466875@qq.com>
2024-02-19 12:24:46 +0800

    minimum server

---
+++ uranus-s/src/lib.rs
@@ -0,0 +1,194 @@
+use std::{io::Cursor, time::Duration};
+
+use anyhow::{anyhow, Result};
+use bytes::{Buf, BytesMut};
+use tokio::{
+    io::{AsyncReadExt, BufWriter},
+    net::{TcpListener, TcpStream},
+    time,
+};
+use tracing::{error, info};
+
+pub async fn run(listener: TcpListener) -> Result<()> {
+    let mut server = Listener { listener };
+
+    tokio::select! {
+        res = server.run() => {
+            if let Err(err) = res {
+                error!(cause = %err, "failed to accept");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Listener {
+    listener: TcpListener,
+}
+
+impl Listener {
+    async fn run(&mut self) -> Result<()> {
+        info!("uranus started to serve requests");
+
+        loop {
+            let socket = self.accept().await?;
+
+            let mut handler = Handler {
+                connection: Connection::new(socket)
+            };
+
+            tokio::spawn(async move {
+                if let Err(err) = handler.run().await {
+                    error!(cause = ?err, "connection error");
+                }
+            });
+        }
+    }
+
+    async fn accept(&mut self) -> Result<TcpStream> {
+        let mut backoff = 1;
+        loop {
+            match self.listener.accept().await {
+                Ok((socket, _)) => return Ok(socket),
+                Err(err) => {
+                    if backoff > 64 {
+                        return Err(err.into());
+                    }
+                }
+            }
+
+            time::sleep(Duration::from_secs(backoff)).await;
+            backoff *= 2;
+        }
+    }
+}
+
+pub struct Handler {
+    connection: Connection,
+}
+
+impl Handler {
+    async fn run(&mut self) -> Result<()> {
+        let frame = tokio::select! {
+            res = self.connection.read_frame() => res?
+        };
+
+        let frame = match frame {
+            Some(frame) => frame,
+            None => return Ok(()),
+        };
+
+        info!("received a frame {:?}", frame);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct Connection {
+    stream: BufWriter<TcpStream>,
+    buffer: BytesMut,
+}
+
+const BUFFER_SIZE: usize = 4 * 1024;
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Connection {
+        Connection {
+            stream: BufWriter::new(socket),
+            buffer: BytesMut::with_capacity(BUFFER_SIZE),
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err(anyhow!("connection reset by peer"));
+                }
+            }
+        }
+    }
+
+    fn parse_frame(&mut self) -> Result<Option<Frame>> {
+        let mut buf = Cursor::new(&self.buffer[..]);
+        match Frame::check(&mut buf) {
+            Ok(None) => Ok(None),
+            Ok(Some(())) => {
+                let len = buf.position() as usize;
+                buf.set_position(0);
+                let frame = Frame::parse(&mut buf)?.unwrap();
+                self.buffer.advance(len);
+                Ok(Some(frame))
+            }
+            Err(e) => Err(e.into()),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Frame {
+    Simple(String),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum FrameError {
+    #[error("This frame is incomplete")]
+    Incomplete,
+}
+
+impl Frame {
+    pub fn check(src: &mut Cursor<&[u8]>) -> Result<Option<()>> {
+        match get_u8(src) {
+            Some(b'+') => {
+                if let Some(_) = get_line(src) {
+                    Ok(Some(()))
+                } else {
+                    Ok(None)
+                }
+            }
+            None => Ok(None),
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn parse(src: &mut Cursor<&[u8]>) -> Result<Option<Frame>> {
+        match get_u8(src) {
+            Some(b'+') => {
+                if let Some(line) = get_line(src).map(|x| x.to_vec()) {
+                    let string = String::from_utf8(line)?;
+                    Ok(Some(Frame::Simple(string)))
+                } else {
+                    Ok(None)
+                }
+            }
+            None => Ok(None),
+            _ => unimplemented!(),
+        }
+    }
+}
+
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Option<&'a [u8]> {
+    let start = src.position() as usize;
+    let end = src.get_ref().len() - 1;
+    for i in start..end {
+        if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
+            src.set_position((i + 2) as u64);
+            return Some(&src.get_ref()[start..i]);
+        }
+    }
+    None
+}
+
+fn get_u8(src: &mut Cursor<&[u8]>) -> Option<u8> {
+    if !src.has_remaining() {
+        return None;
+    }
+    Some(src.get_u8())
+}
---
+++ uranus-s/src/main.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use tokio::net::TcpListener;
+
+const DEFAULT_PORT: u16 = 12322;
+
+#[tokio::main]
+pub async fn main() {
+    smain().await.unwrap();
+}
+
+async fn smain() -> Result<()> {
+    setup_logging()?;
+    let listener = TcpListener::bind(&format!("127.0.0.1:{}", DEFAULT_PORT)).await?;
+    uranus_s::run(listener).await?;
+    Ok(())
+}
+
+fn setup_logging() -> Result<()> {
+    tracing_subscriber::fmt::try_init().map_err(|err| anyhow::anyhow!(err))
+}
